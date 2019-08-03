bb35f1df0ef7c8ea9cc1189ee2f9ba6633843608
Thomas Haschka <haschka@gmail.com>
2019-08-03 22:34:49 +0200

    8bit image viewer

---
+++ 8bit_viewer.c
@@ -0,0 +1,136 @@
+#include<SDL.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#define SDL_MAIN_HANDLED
+
+static inline void fill_texture_buffer_from_8bit(int* tex_buf,
+						 unsigned char* buffer,
+						 int width, int height) {
+
+  int i;
+  int * current_int;
+  char* current_char;
+  for(i=0;i<width*height;i++) {
+    current_int = tex_buf+i;
+    current_char = (char*)current_int;
+
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    
+    current_char[0]=buffer[i];
+    // G
+    current_char[1]=buffer[i];
+    // R
+    current_char[2]=buffer[i];
+    // A
+    current_char[3]=255;
+
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+
+    current_char[0] = 255;
+    current_char[1] = buffer[i];
+    current_char[2] = buffer[i];
+    current_char[3] = buffer[i];
+    
+#endif
+
+  }
+  
+}
+
+unsigned char * load_image_from_file(char* filename,
+				     unsigned int width,
+				     unsigned int height) {
+
+  unsigned char * image = malloc(sizeof(unsigned char)*width*height);
+  int fd = open(filename, O_RDONLY);
+
+  if (fd < 1) {
+    printf("Error opening image file \n");
+    _exit(1);
+  }
+  
+  if (width*height != read(fd, image, width*height)) {
+    printf("Error reading image file \n");
+    _exit(1);
+  }
+  
+  return(image);
+}
+
+
+int main(int argc, char** argv) {
+
+  SDL_Window * my_window = NULL;
+  SDL_Renderer * my_renderer = NULL;
+  SDL_Texture * my_texture;
+  SDL_Event my_event;
+
+  int pitch;
+  
+  int width, height;
+  char* image_8bit;
+  int* image_32bit;
+
+  if(argc != 2) {
+  
+    sscanf(argv[2],"%i", &width);
+    sscanf(argv[3],"%i", &height);
+
+  } else {
+
+    width = 640;
+    height = 400;
+
+  }
+    
+  pitch = width*4;
+  image_32bit=malloc(sizeof(int)*width*height);
+  
+  image_8bit = load_image_from_file(argv[1],width,height);
+
+  SDL_SetMainReady();
+
+  SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
+
+  my_window = SDL_CreateWindow("The Fancy Overlord Image Viewer",
+			       100,100,width,height,0);
+
+  my_renderer = SDL_CreateRenderer(my_window,-1,SDL_RENDERER_SOFTWARE);
+  my_texture = SDL_CreateTexture(my_renderer, SDL_PIXELFORMAT_ARGB8888,
+				 SDL_TEXTUREACCESS_STREAMING,
+				 width,height);
+  
+
+  SDL_LockTexture(my_texture,NULL,(void**)&image_32bit,&pitch);
+  fill_texture_buffer_from_8bit(image_32bit,image_8bit,width,height);
+  SDL_UnlockTexture(my_texture);
+  SDL_RenderCopy(my_renderer,my_texture,NULL,NULL);
+  SDL_RenderPresent(my_renderer);
+
+  while(1) {
+
+    SDL_RenderPresent(my_renderer);
+    
+    while(SDL_PollEvent(&my_event)){
+      switch(my_event.type) {
+      case SDL_QUIT:
+	goto finish;
+	break;
+      case SDL_WINDOWEVENT:
+	if(my_event.window.event == SDL_WINDOWEVENT_CLOSE) {
+	  SDL_Quit();
+	  goto finish;
+	}
+	break;
+      }
+    }
+    SDL_Delay(50);
+  }
+
+ finish:
+  return(0);
+ }
--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # image-viewer-8bit
 view bytes as images - an 8bit raw image viewer
+please see license below before considering using this tool:
+
+This allows you to watch 8 bit images on your computer.
+
+* compilation:
+
+In order to compile and run this software you need to have the sdl2 library
+and its development headers ( i.e. dev packages ) installed. 
+```
+gcc -O2 -march=native `sdl2-config --cflags` 8bit_viewer.c -c -o 8bit_viewer.o`
+gcc 8bit_viewer.o `sdl2-config --libs` -o 8bit_viewer
+```
+
+* usage:
+
+8 bit images can be obtained by using convert, which is part
+of imagemagick for instance,
+
+`convert inputfile.png -depth 8 pgm:- | tail -c 256000 > output.raw`
+
+where 256000 is the size of the image (with by hight) 256000 is in this case
+for a 640x400 image, you have to change this value according to your needs.
+
+you can now view files like:
+```
+./8bit_viewer image.raw 640 400
+```
+where 640 by 400 is your image size.
+
+For fun you can as 8 bit is just the size of a byte watch any file on your
+system as an 8 bit image.
+
+i.e. if you want to see the first 120000 bytes of your harddrive you could
+do something like:
+```
+./8bit_viewer /dev/sda 200 600
+```
+
+* License
+Copyright (c) 2015-2019 Thomas Haschka
+
+This software is provided 'as-is',
+without any express or implied warranty.
+In no event will the authors be held
+liable for any damages arising from
+the use of this software.
+
+Permission is granted to anyone to use
+this software for any purpose
+including commercial applications, and
+to alter it and redistribute it freely,
+subject to the following restrictions:
+
+1.
+The origin of this software must not
+be misrepresented; you must not claim that
+you wrote the original software.
+If you use this software in a product,
+you should cite its source.
+`https://github.com/haschka/image-viewer-8bit.git`
+
+2.
+Altered source version mus plainly marked as such,
+and must not be misrepresented as being the
+original software.
+
+3.
+This notice may not be removed or
+altered from any source distribution.
